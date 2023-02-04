aff2ea2a8ac4b35dae53714f5b219953931fe5e2
Beetroot Paul <paul@beetrootpaul.com>
2023-02-05 00:32:06 +0100

    [TMP 01] public fields + references from one script to another through its component

--- TMP 01/Assets/test scripts/GetInfo.cs
+++ TMP 01/Assets/test scripts/GetInfo.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class GetInfo : MonoBehaviour
 {
-    string myName = "Beetroot";
-    private int myAge = 98;
+    public string myName = "Beetroot";
+    public int myAge = 98;
     float myH = 1.23f;
     bool isSuper = true;
-    
-    
-    
-    
+
+    public DataProviderScript dps;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("My name is: " + myName);
+        PersInfo(myAge);
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    void PersInfo(int n)
+    {
+        Debug.Log("My name is: " + myName + " x " + n);
+        Debug.Log(dps.providedX);
+        Debug.Log(dps.providedY());
     }
 }
