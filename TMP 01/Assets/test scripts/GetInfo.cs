fdde52f6dd8a933a1cc7329a8d233260f08437e5
Beetroot Paul <paul@beetrootpaul.com>
2023-02-04 23:42:41 +0100

    [TMP 01] script attached

--- TMP 01/Assets/test scripts/GetInfo.cs
+++ TMP 01/Assets/test scripts/GetInfo.cs
@@ -7,12 +7,12 @@ public class GetInfo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Starting GetInfo, lol");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("UPDATE-ing GetInfo, rotfl");
     }
 }
