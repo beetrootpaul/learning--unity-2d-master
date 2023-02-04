8001a278b55364ba1f4aa7b58865d2cedf67c10c
Beetroot Paul <paul@beetrootpaul.com>
2023-02-05 00:01:11 +0100

    [TMP 01] variables

--- TMP 01/Assets/test scripts/GetInfo.cs
+++ TMP 01/Assets/test scripts/GetInfo.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GetInfo : MonoBehaviour
 {
+    string myName = "Beetroot";
+    private int myAge = 98;
+    float myH = 1.23f;
+    bool isSuper = true;
+    
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Starting GetInfo, lol");
+        Debug.Log("My name is: " + myName);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("UPDATE-ing GetInfo, rotfl");
     }
 }
